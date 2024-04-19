774ebcf53b8fe0a1e8bba220846311e2f0fb7265
ParisaDarkhal <p.darkhal@gmail.com>
2024-04-19 16:35:30 -0400

    code updated

--- index2.js
+++ index2.js
@@ -16,33 +16,54 @@ import {
 } from "@langchain/core/runnables";
 ///
 
-const openAIApiKey = process.env.OPENAI_API_KEY;
-
 //////////
 // document.addEventListener("submit", (e) => {
 //   e.preventDefault();
 //   progressConverstion();
 // });
 //////////
-//a string holding the phrasing of the prompt
 
-///
-// const embeddings = new OpenAIEmbeddings({ openAIApiKey });
-
-// const sbApiKey = process.env.SUPABASE_KEY;
-// const sbUrl = process.env.SUPABASE_URL;
-// const client = createClient(sbUrl, sbApiKey);
-
-// const vectorStore = new SupabaseVectorStore(embeddings, {
-//   client,
-//   tableName: "documents",
-//   queryName: "match_documents",
-// });
-
-// const retriever = vectorStore.asRetriever();
-
+const openAIApiKey = process.env.OPENAI_API_KEY;
 const llm = new ChatOpenAI({ openAIApiKey });
-///
+
+const standaloneQuestionTemplate = `Given a question, convert it into a standalone question. Question: {question} standalone quesion: `;
+
+//a prompt created using PromptTemplate and the fromTemplate method
+const standaloneQuestionPrompt = PromptTemplate.fromTemplate(
+  standaloneQuestionTemplate
+);
+
+const answerTemplate = `You are a helpful and enthusiastic support bot who can answer a given question about "Retail Mail: Physical Standards for Letters, Cards, Flats, and Parcels" based on context provided. Try to find the answer in the context. If you really don't know the answer, say "I'm sorry, I don't know the answer to that." and direct the questioner to  https://www.usps.com. Don't try to make up an answer. Always speak as if you were chatting to a friend.
+context: {context}
+question:{question}
+answer:`;
+
+const answerPrompt = PromptTemplate.fromTemplate(answerTemplate);
+
+//take the standaloneQuestionPrompt and PIPE model
+const standaloneQuestionChain = standaloneQuestionPrompt
+  .pipe(llm)
+  .pipe(new StringOutputParser());
+
+const retrieverChain = RunnableSequence.from([
+  (prevResult) => prevResult.standalone_question,
+  retriever,
+  combineDocuments,
+]);
+
+const answerChain = answerPrompt.pipe(llm).pipe(new StringOutputParser());
+
+const chain = RunnableSequence.from([
+  {
+    standalone_question: standaloneQuestionChain,
+    original_input: new RunnablePassthrough(),
+  },
+  {
+    context: retrieverChain,
+    question: ({ original_input }) => original_input.question,
+  },
+  answerChain,
+]);
 
 const punctuationTemplate = `Given a sentence, add punctuation where needed.
 sentence:{sentence}
@@ -62,36 +83,6 @@ const translationTemplate = `Given a sentence, translate that sentence into {lan
     `;
 const translationPrompt = PromptTemplate.fromTemplate(translationTemplate);
 
-const standaloneQuestionTemplate = `Given a question, convert it into a standalone question. Question: {question} standalone quesion: `;
-
-//a prompt created using PromptTemplate and the fromTemplate method
-const standaloneQuestionPrompt = PromptTemplate.fromTemplate(
-  standaloneQuestionTemplate
-);
-
-////
-const answerTemplate = `You are a helpful and enthusiastic support bot who can answer a given question about "Retail Mail: Physical Standards for Letters, Cards, Flats, and Parcels" based on context provided. Try to find the answer in the context. If you really don't know the answer, say "I'm sorry, I don't know the answer to that." and direct the questioner to  https://www.usps.com. Don't try to make up an answer. Always speak as if you were chatting to a friend.
-context: {context}
-question:{question}
-answer:`;
-
-const answerPrompt = PromptTemplate.fromTemplate(answerTemplate);
-
-// function combineDocuments(docs) {
-//   // return docs.map((doc) => doc.pageContent).join("\n\n");
-//   if (!Array.isArray(docs)) {
-//     docs = [docs];
-//   }
-//   return docs.map((doc) => (doc && doc.pageContent) || "").join("\n\n");
-// }
-
-// const chain = standaloneQuestionPrompt
-//   .pipe(llm)
-//   .pipe(new StringOutputParser())
-//   .pipe(retriever)
-//   .pipe(combineDocuments)
-//   .pipe(answerPrompt);
-
 const punctuationChain = RunnableSequence.from([
   punctuationPrompt,
   llm,
@@ -110,24 +101,7 @@ const translationChain = RunnableSequence.from([
   new StringOutputParser(),
 ]);
 
-const chain = RunnableSequence.from([
-  {
-    punctuated_sentence: punctuationChain,
-    original_input: new RunnablePassthrough(),
-  },
-  {
-    grammatically_correct_sentence: grammarChain,
-    language: ({ original_input }) => original_input.language,
-  },
-  translationChain,
-]);
 ////
-
-//take the standaloneQuestionPrompt and PIPE model
-// const standaloneQuestionChain = standaloneQuestionPrompt
-//   .pipe(llm)
-//   .pipe(new StringOutputParser())
-//   .pipe(retriever);
 
 //await the response when you INVOKE the chain. remember to pass in a question
 const response = await chain.invoke({
@@ -137,12 +111,26 @@ const response = await chain.invoke({
 
 console.log("response :>> ", response);
 
-// const tweetTemplate = `Generate a promotional tweet for a product, from this product description: {productDesc}`;
 
-// const tweetPrompt = PromptTemplate.fromTemplate(tweetTemplate);
+async function progressConversation() {
+  const userInput = document.getElementById("user-input");
+  const chatbotConversation = document.getElementById(
+    "chatbot-conversation-container"
+  );
+  const question = userInput.value;
+  userInput.value = "";
 
-// const tweetChain = tweetPrompt.pipe(llm);
+  // add human message
+  const newHumanSpeechBubble = document.createElement("div");
+  newHumanSpeechBubble.classList.add("speech", "speech-human");
+  chatbotConversation.appendChild(newHumanSpeechBubble);
+  newHumanSpeechBubble.textContent = question;
+  chatbotConversation.scrollTop = chatbotConversation.scrollHeight;
 
-// const response = await tweetChain.invoke({ productDesc: "parcel" });
-
-// console.log("response :>> ", response.content);
+  // add AI message
+  const newAiSpeechBubble = document.createElement("div");
+  newAiSpeechBubble.classList.add("speech", "speech-ai");
+  chatbotConversation.appendChild(newAiSpeechBubble);
+  newAiSpeechBubble.textContent = result;
+  chatbotConversation.scrollTop = chatbotConversation.scrollHeight;
+}
