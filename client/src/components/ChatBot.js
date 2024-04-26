f4b6bcda0b606811602f5ce386273d565d6af78c
ParisaDarkhal <p.darkhal@gmail.com>
2024-04-26 14:31:47 -0400

    css  improved

--- client/src/components/ChatBot.js
+++ client/src/components/ChatBot.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "../App.css";
 import Logo from "./images/Logo.png";
 import SendIcon from "./images/send.svg";
@@ -7,6 +7,20 @@ import axios from "axios";
 const ChatBot = () => {
   const [userInput, setUserInput] = useState("");
   const [chatMessages, setChatMessages] = useState([]);
+  const chatContainerRef = useRef(null);
+
+  // Function to scroll to the bottom of the chat container
+  const scrollToBottom = () => {
+    if (chatContainerRef.current) {
+      chatContainerRef.current.scrollTop =
+        chatContainerRef.current.scrollHeight;
+    }
+  };
+
+  // Scroll to bottom on component update (when messages change)
+  useEffect(() => {
+    scrollToBottom();
+  }, [chatMessages]); // Assuming 'messages' is the array of chat messages
 
   const handleChange = (e) => {
     setUserInput(e.target.value);
@@ -54,11 +68,12 @@ const ChatBot = () => {
         <section className="chatbot-container">
           <div className="chatbot-header">
             <img src={Logo} className="logo" alt="Logo" />
-            <p className="sub-heading">Knoledge Bank</p>
+            {/* <p className="sub-heading">Knoledge Bank</p> */}
           </div>
           <div
             className="chatbot-conversation-container"
             id="chatbot-conversation-container"
+            ref={chatContainerRef}
           >
             {/* render chat messages */}
             {chatMessages.map((message, index) => {
